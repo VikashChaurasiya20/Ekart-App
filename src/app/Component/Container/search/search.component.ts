b303e65950970b7e53ac297da75aa4330f9b6e05
VikashChaurasiya20 <116479356+VikashChaurasiya20@users.noreply.github.com>
2024-06-06 17:28:16 +0530

    eKart-app

---
+++ src/app/Component/Container/search/search.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  searchText: string = '';
+
+  updateSearchText(event: any){
+      this.searchText = event.target.value;
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Component/header/header.component';
+import { TopHeaderComponent } from './Component/top-header/top-header.component';
+import { TopMenuComponent } from './Component/header/top-menu/top-menu.component';
+import { MainMenuComponent } from './Component/header/main-menu/main-menu.component';
+import { ContainerComponent } from './Component/Container/container.component';
+import { SearchComponent } from './Component/Container/search/search.component';
+import { ProductListComponent } from './Component/Container/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopHeaderComponent,
+    TopMenuComponent,
+    MainMenuComponent,
+    ContainerComponent,
+    SearchComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
